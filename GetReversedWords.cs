610ec132557fae42c46c85969b34d20a42a9a4ab
David Vesely <davidvesely@gmail.com>
2013-11-06 17:19:13 +0200

    Geometry added

---
+++ GetReversedWords.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.GetReversedWords
+{
+    class GetReversedWords
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("Uglies code ever writen by me"));
+        }
+
+        private static string ReverseWords(string input)
+        {
+            int inputLenght = 0;
+
+            try
+            {
+                while (input[inputLenght++] != '1') ;
+                
+            }
+            catch (IndexOutOfRangeException  e)
+            {
+            }
+
+            string result = String.Empty;
+
+            for (int i = 0; i < inputLenght - 1; i++)
+            {
+                string word = String.Empty;
+                while (i < inputLenght - 1 && input[i] != ' ')
+                {
+                    word += input[i];
+                    i++;
+                }
+                if ((i < inputLenght - 1 && input[i] == ' '))
+                {
+                    word = " " + word;
+                }
+                result = word + result;                
+            }
+
+
+            return result;
+        }
+    }
+}
